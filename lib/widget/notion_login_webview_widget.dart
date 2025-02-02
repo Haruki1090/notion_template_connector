1037b212fe10f2a0ee4f6e9b89fed8984d11345d
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-02 15:45:58 +0900

    feat: Add flutter_dotenv and update webview settings
    - Added `flutter_dotenv` package to `pubspec.yaml` for environment variable management.
    - Added `.env` file in `pubspec.yaml`.
    - Updated `notion_login_webview_widget.dart` to use `Env.authUrl` and set the latest browser user agent.
    - Added webView Controller in `notion_login_webview_widget.dart` for webView.
    - Modified `notion_login_webview_widget.dart` to use deeplink with custom URL scheme.

--- lib/widget/notion_login_webview_widget.dart
+++ lib/widget/notion_login_webview_widget.dart
@@ -9,7 +9,7 @@ import '../env/env.dart';
 import '../provider/notion_auth_provider.dart';
 
 class NotionLoginWebviewWidget extends ConsumerStatefulWidget {
-  const NotionLoginWebviewWidget({super.key});
+  const NotionLoginWebviewWidget({Key? key}) : super(key: key);
 
   @override
   ConsumerState<NotionLoginWebviewWidget> createState() =>
@@ -18,6 +18,7 @@ class NotionLoginWebviewWidget extends ConsumerStatefulWidget {
 
 class _NotionLoginWebviewWidgetState
     extends ConsumerState<NotionLoginWebviewWidget> {
+  InAppWebViewController? _webViewController;
   // カスタムURLスキーム（例: "notionsample"）
   final String customScheme = "notionsample";
 
@@ -26,24 +27,28 @@ class _NotionLoginWebviewWidgetState
     return Scaffold(
       appBar: AppBar(title: const Text('Notion Login')),
       body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri((Env.authUrl).toString())),
+        initialUrlRequest:
+            URLRequest(url: WebUri(Uri.parse(Env.authUrl).toString())),
         initialOptions: InAppWebViewGroupOptions(
           crossPlatform: InAppWebViewOptions(
             useShouldOverrideUrlLoading: true,
             javaScriptEnabled: true,
+            // 最新のブラウザを模倣するUser Agentに更新
             userAgent: Platform.isIOS
-                ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1'
-                : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36',
+                ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1'
+                : 'Mozilla/5.0 (Linux; Android 12; Pixel 5 Build/SQ3A.220705.003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.97 Mobile Safari/537.36',
           ),
           android: AndroidInAppWebViewOptions(
             disableDefaultErrorPage: true,
           ),
         ),
-        onWebViewCreated: (controller) {},
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
         shouldOverrideUrlLoading: (controller, navigationAction) async {
           final uri = navigationAction.request.url;
           if (uri != null && uri.scheme == customScheme) {
-            // カスタムURLスキームによる deeplink 受信
+            // カスタムURLスキームによるdeeplink受信
             final code = uri.queryParameters['code'];
             if (code != null) {
               // 認可コードをアクセストークンに交換する
